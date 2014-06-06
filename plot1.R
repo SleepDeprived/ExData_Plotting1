dd18b37b20d2fcbb2b409aa00d5e9bfbe10d3d9e
Tom Metzger <tom@tmetzger.com>
2014-06-05 21:45:20 -0400

    added plot scripts that I started on another computer and data for plotting

---
+++ plot1.R
@@ -0,0 +1,20 @@
+
+createPlot1 <- function() {
+  
+  data <- read.table("household_power_consumption.txt", header = TRUE, na.strings = "?", stringsAsFactors = FALSE, sep = ";")
+  colnames(data) <- tolower(colnames(data))
+  
+  data$date_time <- strptime(paste(data$date, data$time, sep = " "), format = "%d/%m/%Y %H:%M:%S")
+  data$date <- as.Date(data$date, format="%d/%m/%Y")
+  
+  powerData <- data[data$date >= as.Date("2007-02-01", format = "%Y-%m-%d"), ]
+  powerData <- powerData[powerData$date <= as.Date("2007-02-02", format = "%Y-%m-%d"), ]
+  rm(data)
+  
+  hist(powerData$global_active_power, col="red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+  png("plot1.png", width = 480, height = 480, units="px")
+  
+}
+
+
---
+++ plot4.R
@@ -0,0 +1,19 @@
+createPlot4 <- function() {
+  
+  data <- read.table("household_power_consumption.txt", header = TRUE, na.strings = "?", stringsAsFactors = FALSE, sep = ";")
+  colnames(data) <- tolower(colnames(data))
+  
+  data$date_time <- strptime(paste(data$date, data$time, sep = " "), format = "%d/%m/%Y %H:%M:%S")
+  data$date <- as.Date(data$date, format="%d/%m/%Y")
+  
+  powerData <- data[data$date >= as.Date("2007-02-01", format = "%Y-%m-%d"), ]
+  powerData <- powerData[powerData$date <= as.Date("2007-02-02", format = "%Y-%m-%d"), ]
+  rm(data)
+  
+  # add code here to make plot
+  
+  # add code here to make PNG  
+  
+}
+
+
